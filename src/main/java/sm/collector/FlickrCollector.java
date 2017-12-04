cee09bae1eefa5d9c605c233ec47e03805708f37
hiba <hiba.enis@gmail.com>
2017-12-04 10:57:02 +0100

    Rectification of YouTube and Flickr Collector

--- src/main/java/sm/collector/FlickrCollector.java
+++ src/main/java/sm/collector/FlickrCollector.java
@@ -13,50 +13,58 @@ import com.flickr4java.flickr.test.TestInterface;
 import com.flickr4java.flickr.util.XMLUtilities;
 import groovy.json.JsonBuilder;
 import org.w3c.dom.Element;
+import sm.collector.entity.Content;
+import sm.collector.entity.Post;
+import sm.collector.entity.Profile;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class FlickrCollector {
-// private  final Transport transportAPI ;
-public static void main (String [] args)throws FlickrException{
+public class FlickrCollector extends Collector {
+    // private  final Transport transportAPI ;
+    public List<Profile> collectProfiles(String keyword) {
+        return null;
+    }
 
-    String apiKey = "1d16520343c27f3f2b0f973b1d447e47";
-    String sharedSecret = "91844af3aa9d6069";
-    Flickr f = new Flickr(apiKey, sharedSecret, new REST());
-    TestInterface testInterface = f.getTestInterface();
-    java.util.Collection results = testInterface.echo(Collections.EMPTY_MAP);
-    //initialize SearchParameter object, this object stores the search keyword
-    SearchParameters searchParams=new SearchParameters();
-    searchParams.setSort(SearchParameters.INTERESTINGNESS_DESC);
-    //Create tag keyword array
-    String[] tags=new String[]{"Dog","Doberman"};
-    searchParams.setTags(tags);
-
-    //Initialize PhotosInterface object
-    PhotosInterface photosInterface=f.getPhotosInterface();
-
-    //Execute search with entered tags
-    // PhotoList photoList=null;
-    PhotoList photoList=photosInterface.search(searchParams,20,1);
-    System.out.println("here");
-    //get search result and fetch the photo object and get small square imag's url
-    if(photoList!=null){
-        System.out.println("photoList "+ photoList.getPages());
-        //Get search result and check the size of photo result
-        for(int i = 0; i<photoList.size();i++){
-         Photo photo=(Photo)photoList.get(i);
-         System.out.println( "*********** The " +i + " photo ***************");
-         System.out.println ("url: " + photo.getUrl());
-         System.out.println(  "description: " +photo.getDescription());
-         System.out.println(  "id: " +photo.getId());
-         System.out.println( "title: " + photo.getTitle());
-         System.out.println( "comment: " + photo.getComments());
-         System.out.println( "owner: " + photo.getOwner());
+    public List<Post> collectPosts(String queryTerm) {
+        List<Post> posts = new LinkedList<>();
+        Flickr flickr = authentification();
+        TestInterface testInterface = flickr.getTestInterface();
+        try {
+            java.util.Collection results = testInterface.echo(Collections.EMPTY_MAP);
+        } catch (FlickrException e) {
+            System.err.println("There was an IO error: " + e.getCause() + " : "
+                    + e.getMessage());
+        }
+        //initialize SearchParameter object, this object stores the search keyword
+        SearchParameters searchParams = new SearchParameters();
+        searchParams.setSort(SearchParameters.INTERESTINGNESS_DESC);
+        //Create tag keyword array
+        String[] tags = new String[]{queryTerm};
+        searchParams.setTags(tags);
+        //Initialize PhotosInterface object
+        PhotosInterface photosInterface = flickr.getPhotosInterface();
+        //Execute search with entered tags
+        // PhotoList photoList=null;
+        try {
+            PhotoList photoList = photosInterface.search(searchParams, 20, 1);
+            for (int i = 0; i < photoList.size(); i++) {
+                posts.add(new Post(Content.Type.FLICKR, (Photo) photoList.get(i)));
+            }
+        } catch (FlickrException e) {
+            System.err.println("There was an IO error: " + e.getCause() + " : "
+                    + e.getMessage());
         }
 
-             }
-     }
+
+        return posts;
+    }
+
+    public Flickr authentification() {
+        String apiKey = "1d16520343c27f3f2b0f973b1d447e47";
+        String sharedSecret = "91844af3aa9d6069";
+        Flickr flickr = new Flickr(apiKey, sharedSecret, new REST());
+        return flickr;
+    }
+
 }
 
